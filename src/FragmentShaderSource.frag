d666a6ad0d72c8913cc6e8a5ace81e1726d95e46
kaczor6418 <k.kaczynski97@gmail.com>
2021-01-27 17:30:45 +0100

    wip(webgl-service): trying rendering stuff

--- src/FragmentShaderSource.frag
+++ src/FragmentShaderSource.frag
@@ -4,10 +4,17 @@
 // to pick one. highp is a good default. It means "high precision"
 precision highp float;
 
+//color from vertex shader
+in vec4 color;
+
+// color of vertex node
+uniform vec4 u_color;
+
+
 // we need to declare an output for the fragment shader
 out vec4 outColor;
 
 void main() {
     // Just set the output to a constant reddish-purple
-    outColor = vec4(1, 0, 0.5, 1);
+    outColor = color == vec4(0, 0, 0, 1) ? u_color : color;
 }
--- src/services/WebGLService/WebGLService.ts
+++ src/services/WebGLService/WebGLService.ts
@@ -16,27 +16,50 @@ export class WebGLService {
     this.fragmentShader = this.createShader(this.gl.FRAGMENT_SHADER, fragmentShaderSource);
     this.program = this.createProgram(this.vertexShader, this.fragmentShader);
     this.positionAttributeLocation = this.gl.getAttribLocation(this.program, 'a_position');
-    this.positionBuffer = <WebGLBuffer>this.gl.createBuffer();
-    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.positionBuffer);
-    const pointA = [0, 0];
-    const pointB = [0, 0.5];
-    const pointC = [0.7, 0];
-    const positions = [...pointA, ...pointB, ...pointC];
-    this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(positions), this.gl.STATIC_DRAW);
+    const colorAttributeLocation = this.gl.getAttribLocation(this.program, 'a_color');
+    const resolutionUniformLocation = <WebGLUniformLocation>this.gl.getUniformLocation(this.program, 'u_resolution');
+    const colorLocation = <WebGLUniformLocation>this.gl.getUniformLocation(this.program, 'u_color');
+
     this.vao = <WebGLVertexArrayObject>this.gl.createVertexArray();
     this.gl.bindVertexArray(this.vao);
+
+    this.positionBuffer = <WebGLBuffer>this.gl.createBuffer();
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.positionBuffer); // 0 → default no buffer / unbind
+    const pointA = [10, 20];
+    const pointB = [80, 20];
+    const pointC = [10, 30];
+    const pointD = [10, 30];
+    const pointE = [80, 20];
+    const pointF = [80, 30];
+    const positions = [...pointA, ...pointB, ...pointC, ...pointD, ...pointE, ...pointF];
+    this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(positions), this.gl.STATIC_DRAW);
     this.gl.enableVertexAttribArray(this.positionAttributeLocation);
-    this.gl.vertexAttribPointer(this.positionAttributeLocation, 2, this.gl.FLOAT, false, 0, 0);
+    this.gl.enableVertexAttribArray(colorAttributeLocation);
+    this.gl.vertexAttribPointer(this.positionAttributeLocation, 2, this.gl.FLOAT, false, 0, 0); // size → how many parameters of each set should take 2 (means x and y) 3 (x y and z)
+
+    const colors = [1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1];
+    const colorBuffer = <WebGLBuffer>this.gl.createBuffer();
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, colorBuffer);
+    this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(colors), this.gl.STATIC_DRAW);
+    this.gl.enableVertexAttribArray(colorAttributeLocation);
+    this.gl.vertexAttribPointer(colorAttributeLocation, 4, this.gl.FLOAT, false, 0, 0);
+
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, null);
+    this.gl.bindVertexArray(null);
+
+    this.resizeCanvasToDisplaySize(canvas);
     this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
     this.clearCanvas();
     this.gl.useProgram(this.program);
     this.gl.bindVertexArray(this.vao);
-    this.gl.drawArrays(this.gl.TRIANGLES, 0, 3);
+    this.gl.uniform2f(resolutionUniformLocation, this.gl.canvas.width, this.gl.canvas.height);
+    this.gl.uniform4f(colorLocation, 1, 0.5, 0.5, 1);
+    this.gl.drawArrays(this.gl.TRIANGLES, 0, 6); // count → how many points should draw
   }
 
   private clearCanvas(): void {
     this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
   }
 
   private createShader(type: GLenum, source: string): WebGLShader {
@@ -70,5 +93,16 @@ export class WebGLService {
       throw new Error('Unable to initialize WebGL. Your browser or machine may not support it.');
     }
     return context;
+  }
+
+  private resizeCanvasToDisplaySize(canvas: HTMLCanvasElement, multiplier = 1): boolean {
+    const width = (canvas.clientWidth * multiplier) | 0;
+    const height = (canvas.clientHeight * multiplier) | 0;
+    if (canvas.width !== width || canvas.height !== height) {
+      canvas.width = width;
+      canvas.height = height;
+      return true;
+    }
+    return false;
   }
 }
