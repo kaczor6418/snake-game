e36f23ac90a7a779dfc4d7cc8ae8cae9de1e55f6
kaczor6418 <k.kaczynski97@gmail.com>
2021-02-06 19:02:11 +0100

    test(game-view): add game view tests and we gl mock

---
+++ src/core/GameView/GameView.unit.test.ts
@@ -0,0 +1,27 @@
+import { GameView } from './GameView';
+import { webGLServiceMock } from '../../services/WebGLService/__mocks__/web-gl-service';
+
+describe(GameView.name, () => {
+  let gameView: GameView;
+  const fieldWidth = 10;
+  const fieldHeight = 10;
+  beforeEach(() => {
+    gameView = new GameView(webGLServiceMock, fieldWidth, fieldHeight);
+  });
+  describe(GameView.prototype['convertToPositionOnCanvas'].name, () => {
+    test('should convert row and column index to canvas position', () => {
+      const x = 2;
+      const y = 3;
+      const [xPosition, yPosition] = gameView['convertToPositionOnCanvas']({ x, y });
+      expect(xPosition).toBe(x * fieldWidth);
+      expect(yPosition).toBe(y * fieldHeight);
+    });
+    test('should return same position for row and column index 0', () => {
+      const x = 0;
+      const y = 0;
+      const [xPosition, yPosition] = gameView['convertToPositionOnCanvas']({ x, y });
+      expect(xPosition).toBe(x);
+      expect(yPosition).toBe(y);
+    });
+  });
+});
