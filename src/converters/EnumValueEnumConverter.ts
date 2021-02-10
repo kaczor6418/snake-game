9c62d582c704238ab1364351698d94491446afc4
kaczor6418 <k.kaczynski97@gmail.com>
2021-02-10 01:38:47 +0100

    feat(enum-converter): add enum converter implementation

---
+++ src/converters/EnumValueEnumConverter.ts
@@ -0,0 +1,21 @@
+import { UTILS } from '../common/Utils/UTILS';
+import { EnumKeyOrValueNotFound } from '../errors/EnumKeyOrValueNotFound';
+
+export class EnumValueEnumConverter {
+  public static toEnumFromKey<T>(key: string, enumDeclaration: Record<string, T>): T {
+    const convertedEnum: T = enumDeclaration[key];
+    if (UTILS.isNullOrUndefined(convertedEnum)) {
+      throw new EnumKeyOrValueNotFound(`Key: **${key}** doesn't exist in this enum!`);
+    }
+    return convertedEnum;
+  }
+
+  public static toEnumFromValue<T>(value: string | number, enumDeclaration: Record<string, T>): T {
+    const enumEntries: Array<[string, string | unknown]> = Object.entries(enumDeclaration);
+    const enumSingleItem: [string, string | unknown] | undefined = enumEntries.find((enumSet) => enumSet.includes(value));
+    if (UTILS.isNullOrUndefined(enumSingleItem)) {
+      throw new EnumKeyOrValueNotFound(`Value: **${value}** doesn't exist in this enum!`);
+    }
+    return enumDeclaration[enumSingleItem[0]];
+  }
+}
---
+++ src/errors/EnumKeyOrValueNotFound.ts
@@ -0,0 +1,6 @@
+export class EnumKeyOrValueNotFound extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = EnumKeyOrValueNotFound.name;
+  }
+}
