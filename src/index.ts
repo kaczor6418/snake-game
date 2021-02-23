5c0e93f9abe7936433cde70b08ab63ea516566dc
kaczor6418 <k.kaczynski97@gmail.com>
2021-02-23 22:14:54 +0100

    fix(project): fix webpack issue and remove never used gl-matrix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import * as styles from './styles/main.scss';
+import styles from './styles/main.scss';
 export { App } from './App';
 const rootStyleWrapper: HTMLStyleElement = document.createElement('style');
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 rootStyleWrapper.innerHTML = styles;
 document.head.appendChild(rootStyleWrapper);
