2d92b00a167bdb0ed97e03867f15e1a9d0a42009
kaczor6418 <k.kaczynski97@gmail.com>
2021-05-31 00:24:00 +0200

    chore(network-models): add webpack plugin to allow serve tensorflow models and weights

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -14,7 +15,7 @@ module.exports = {
       {
         test: /\.ts$/,
         use: 'ts-loader',
-        exclude: /node_modules/,
+        exclude: /node_modules/
       },
       {
         test: /\.svg$/,
@@ -39,9 +40,12 @@ module.exports = {
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: path.resolve(__dirname, 'src/index.html'),
       filename: 'index.html',
+      template: path.resolve(__dirname, 'src/index.html'),
       favicon: path.resolve(__dirname, 'src/favicon.ico')
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: path.resolve(__dirname, 'assets'), to: 'assets' }]
     })
   ]
 };
